6d373a592c746f3d4f2f62ccc9e4f3b4aec58d4a
Strilets Glib FI41mn <181478164+Strilets-Glib-FI41mn@users.noreply.github.com>
2025-11-26 21:07:40 +0200

    using naive sorting still

--- burrows_wheeler_transform/src/lib.rs
+++ burrows_wheeler_transform/src/lib.rs
@@ -2,50 +2,71 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-pub fn create_all_rotations<T>(input: &[T]) -> Vec<(Vec<&T>, bool)>{
+pub fn create_all_rotations<T: Clone>(input: &[T]) -> Vec<(Vec<&T>, bool)>{
     let lenth = input.len();
-    let ps_sl: Vec<&T> = input.iter().collect();
+    let ps_sl: Vec<_> = input.iter().collect();
+    // let ps_sl = input.clone();
     let table = (0..lenth).into_iter().map(|
         i| {
             let mut c = ps_sl.clone();
             c.rotate_left(i);
-        (c, i == 0)
+            (c, 0 == i)
         }).collect();
     table
 }
-pub fn bwt_encode<T>(input: &[T]) -> (Vec<&T>, usize)
-where for<'a> &'a Vec<&'a T>: Ord
+pub fn bwt_encode<T>(input: &[T]) -> (Vec<T>, usize)
+// where [T; N]: rdxsort::RdxSortTemplate,
+where T: Clone + PartialEq + Ord
+// for<'a> Vec<Vec<&'a T>>: rdxsort::RdxSort
 {
     let lenth = input.len();
     let mut table = create_all_rotations(input);
-    table.sort_by(|(a, _), (b, _)| a.cmp(&b));
-    let o = table.iter().position(|(_, i)| *i == true).unwrap();
-    let result:  Vec<_> =table.iter().map(|(t, _)| {
-        t[lenth - 1]
+    table.sort();
+    // table.sort_by(|(a, _), (b, _)| a.cmp(&b));
+    // table.rdxsort();
+    println!("sorted ok");
+    let o = table.iter().position(|(_, b)| *b).unwrap();
+    let result:  Vec<_> =table.iter().map(|t| {
+        t.0[lenth - 1].clone()
     }).collect();
     (result, o)
 }
+
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn sorted_table(){
-
-        let a: Vec<u8>= vec![3, 4, 2, 1];
+        let a = [3, 4, 2, 1];
         let mut b = create_all_rotations(&a);
         b.sort();
         println!("Sorted? table {b:?}");
-        assert_eq!(b[0].0, &[&1, &3, &4, &2])
+        assert_eq!(b[0].0, [&1, &3, &4, &2])
     }
     #[test]
-    fn sorted_text(){
-        let thing = "ананас";
-        let thing_: Vec<_> = thing.chars().collect();
-        let res = bwt_encode(&thing_);
+    fn sorted_test(){
+        let thing: Vec<_> = "ананас".chars().collect();
+        // let thing = ['а', 'н', 'а', 'н', 'а', 'с'];
+        // let thing = [99u8, 2, 5, 7, 14];
+        // println!("{thing:?}");
+        let res = bwt_encode(&thing);
         println!("res: {res:?}");
         let expected_: Vec<_> = "сннааа".chars().collect();
-        let expected = expected_.iter().collect();
-        assert_eq!((expected, 0usize), res);
+        assert_eq!((expected_, 0usize), res);
+    }
+    #[test]
+    fn sorted_u8(){
+        // let thing: &[char] = "ананас".into();
+        // let thing = ['а', 'н', 'а', 'н', 'а', 'с'];
+        let thing = [99u8, 2, 5, 7, 14];
+        println!("{:?}", create_all_rotations(&thing));
+        let res = bwt_encode(&thing);
+        println!("res: {res:?}");
+        // let expected_: Vec<_> = "сннааа".chars().collect();
+        let expected_ = vec![99, 2, 5, 7, 14];
+        assert_eq!((expected_, 4usize), res);
     }
 }
