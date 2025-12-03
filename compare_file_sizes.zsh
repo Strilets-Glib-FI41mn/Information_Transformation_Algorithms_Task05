8437533e6c4c6bc5afe9a4e3b6edb23ba1538b88
Aultus Defora <181478164+Strilets-Glib-FI41mn@users.noreply.github.com>
2025-12-03 17:01:20 +0200

    Create compare_file_sizes.zsh

---
+++ compare_file_sizes.zsh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the directories
+folder1=$1
+folder2=$2
+
+# Iterate over files in the first folder
+for file in "$folder1"/*; do
+    filename=$(basename "$file")
+    #echo $filename
+    # Check for multiple appended extensions
+    for ext in "$folder2"/"$filename".*; do
+        if [ -e "$ext" ]; then
+            size1=$(wc -c < "$file")
+            size2=$(wc -c < "$ext")
+            echo "Comparing: $filename"
+            echo "Size in $folder1: $size1 bytes"
+            echo "Size in $folder2: $size2 bytes"
+            echo "scale=4; $size1 / $size2" | bc
+            echo ""
+        fi
+    done
+done
